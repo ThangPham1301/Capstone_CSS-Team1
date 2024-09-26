45579a055a4b4c04199448965967a63ff00aeb0a
Thang-Pham <hahanh29911301@gmail.com>
2024-09-26 23:38:28 +0700

    hoan thien testimonials

---
+++ Public/js/slick.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.slick__Testimonials').slick({
+        slidesToShow: 4,
+        Infinity:true,
+        arrows: false,
+        autoplay:true,
+        autoplaySpeed: 2000,
+        draggable: false,
+    });
+  });
